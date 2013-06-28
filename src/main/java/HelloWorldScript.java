be806f907eea95d20a3ccfbde56a54bbdf4918e2
Dan Erickson <derickson82@gmail.com>
2013-06-27 23:02:37 -0600

    Additional changes

--- src/main/java/HelloWorldScript.java
+++ src/main/java/HelloWorldScript.java
@@ -1,3 +1,10 @@
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
 /**
  * Created with IntelliJ IDEA.
  * User: Dan
@@ -6,4 +13,34 @@
  * To change this template use File | Settings | File Templates.
  */
 public class HelloWorldScript {
+
+    public static void main(String... args) {
+        ScriptEngineManager scm = new ScriptEngineManager();
+
+        scm.getEngineFactories().forEach(engineFactory -> {
+            System.out.println(String.format("Engine Factory: %s", engineFactory));
+            System.out.println(String.format("\tLanguage: %s", engineFactory.getLanguageName()));
+            engineFactory.getExtensions().forEach(extension -> {
+                System.out.println(String.format("\tExtension %s", extension));
+            });
+
+            engineFactory.getNames().forEach(name -> {
+                System.out.println("\tName: " + name);
+            });
+        });
+
+        ScriptEngine jsEngine = scm.getEngineByName("js");
+
+        try {
+            jsEngine.eval("print('Hello World');");
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+
+        try (Reader helloWorldScript = new InputStreamReader(HelloWorldScript.class.getResourceAsStream("/HelloWorld.js"))) {
+            jsEngine.eval(helloWorldScript);
+        } catch (ScriptException | IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
