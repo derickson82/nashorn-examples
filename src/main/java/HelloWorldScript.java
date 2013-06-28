cae4ff13916392b5fb8d621416dfa4952e0b28fe
Dan Erickson <derickson82@gmail.com>
2013-06-27 22:59:55 -0600

    Initial checkin, with hello world scripts

---
+++ src/main/java/HelloWorldScript.java
@@ -0,0 +1,9 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dan
+ * Date: 6/27/13
+ * Time: 10:17 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class HelloWorldScript {
+}
